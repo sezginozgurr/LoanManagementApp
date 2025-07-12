78966946bb62deb9abde1cf7c8eca8752fd578ba
Özgür Can <sezginzgurr@gmail.com>
2025-07-12 17:32:52 +0300

    first commit with love <3

---
+++ app/src/main/java/com/loanmanagementapp/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.loanmanagementapp
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import com.loanmanagementapp.data.LoanRepository
+import com.loanmanagementapp.ui.screen.HomeScreen
+import com.loanmanagementapp.ui.screen.LoginScreen
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           LoanApp()
+        }
+    }
+}
+
+@Composable
+fun LoanApp() {
+    var isLoggedIn by remember { mutableStateOf(false) }
+
+    if (isLoggedIn) {
+        HomeScreen()
+    } else {
+        LoginScreen(onLoginSuccess = { isLoggedIn = true })
+    }
+}
